846ea9ae8fb5ccfb51da1dc38a25164198b37ee4
Maty2442 <80933772+Maty2442@users.noreply.github.com>
2023-05-31 23:13:42 -0300

    Add files via upload

---
+++ 01-CalcularP/Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <math.h>
+/* Comentario en más
+de una línea */
+int main()
+{
+// cout y endl están declaradas en iostream
+ using namespace std; // no hace falta poner std:: as ique nda usa esto que s mas facil
+ int n,i;
+ double pi{0};
+n=900000;
+
+for(i=0;i<n;++i){
+pi+= (pow((-1),i))/(2*i+1);
+}
+pi*=4;
+cout.precision(7);
+cout<<"El numero Pi es mas o menos "<<pi<<endl;
+
+
+return 0;
+
+
+}
+
